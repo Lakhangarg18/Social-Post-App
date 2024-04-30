5036304d20ee4c90d6c37da99d75ffb885ebda35
Lakhan Garg <113020547+Lakhangarg18@users.noreply.github.com>
2024-04-30 20:57:47 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+
+
+import { useState } from "react";
+import PostListProvider from "./store/post-list-store";
+function App() {
+  const [selectedTab, setSelectedTab] = useState("Create Post");
+  return (
+    <PostListProvider>
+      <div className="app-container">
+        <SideBar
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
+        ></SideBar>
+        <div className="content">
+          <Header />
+          {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+
+          <Footer />
+
+        </div>
+      </div>
+    </PostListProvider>
+  );
+}
+
+export default App;
